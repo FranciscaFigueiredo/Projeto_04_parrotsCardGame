47138d7eee82282d637bf7befeeba62b01e1c46e
FranciscaFigueiredo <fraan928@gmail.com>
2021-07-29 15:12:44 -0300

    Accepting only even numbers between 4 and 14

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
-let quantidadeDeCartas = prompt("Qual a quantidade de cartas que deseja jogar?");
+let quantidadeDeCartas = "";
 
+validarQuantidade();
 
+function validarQuantidade() {
+    quantidadeDeCartas = prompt("Qual a quantidade de cartas que deseja jogar?");
+    
+    if (quantidadeDeCartas < 4 || quantidadeDeCartas > 14 || quantidadeDeCartas % 2 !== 0) {
+        alert("A quantidade de cartas tem que ser um valor PAR entre 4 e 14");
+        validarQuantidade();
+    }
+}
