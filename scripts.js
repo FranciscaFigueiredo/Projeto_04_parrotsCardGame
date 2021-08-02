dc17e9319a83915d7042672d630304f8c20d9060
FranciscaFigueiredo <fraan928@gmail.com>
2021-08-02 17:24:25 -0300

    Adjusted

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 let quantidadeDeCartas = "";
 const baralho = [];
 let cartasNaoRepetitas;
-let viradas = -1;
+let viradas = 0;
 let id = 0;
 let id2 = 0;
+let jogadas = 0;
+let acertos = 0;
 
 const cartas = [
     '<img src="assets/bobrossparrot.gif" alt="1" />',
@@ -51,27 +53,85 @@ function comparador() {
 
 function colocarCartas(carta, indice) {
     const container = document.querySelector(".container");
-     const card = container.querySelector(".card");
 
     for (let i = 0; i < quantidadeDeCartas; i++) {
-        container.innerHTML += `<div class="card" onclick="virarCarta(this);"><img src="assets/front.png" alt="front"><div class="carta-virada">${baralho[i]}</div></div>`;
+        container.innerHTML += `<div class="card" onclick="virarCarta(this);"><div class="front"><img src="assets/front.png"></div><div class="carta-virada">${baralho[i]}</div></div>`;
     }
 }
 
 function virarCarta(card) {
+    card.classList.toggle("selecionada")
+    const imagem = card.querySelector(".front");
     const virar = card.querySelector(".carta-virada")
+    let primeira = 0;
+    let segunda = 0;
+    let imagem1;
+    let imagem2;
     let idCard = 0;
-    let imagem = card.querySelector(".card img");
-    imagem.classList.toggle("esconder");
     
-    virar.classList.toggle("selecionada");
-    viradas++;
+    if (viradas !== 2) {
+        imagem.classList.toggle("esconder");
+        
+        virar.classList.toggle("selecionada");
+        viradas++;
 
-    idCard = card.querySelector(".selecionada alt");
-        id = idCard;
-    console.log(idCard);    
+        idCard = virar.querySelector(".selecionada img").alt;
+        console.log(idCard)
+        if (viradas === 1) {
+            imagem1 = card.querySelector(".selecionada div");
+            virar.classList.toggle("primeira");
+            
+            primeira = virar.querySelector(".primeira");
+            id = idCard;
+        } else if (viradas === 2) {
+            imagem2 = card.querySelector(".selecionada div");
+            virar.classList.toggle("segunda");
+            
+            segunda = virar.querySelector(".segunda");
+            id2 = idCard;
+            if (!verificarIguais(id, id2, card)) {
+                setTimeout(function () {
+                    imagem1.classList.toggle("esconder");
+                    imagem2.classList.toggle("esconder")
+                    
+                    primeira.classList.toggle("esconder");                
+                    segunda.classList.toggle("esconder");
+                }, 1000);
+                viradas = 0;
+            }
+        }
+    }
+    
 }
-
-function verificarIguais(card) {
-
+let primeiraCarta;
+    let segundaCarta;
+function verificarIguais(id, id2, card) {
+    const carta = card.querySelector(".selecionada");
+    primeiraCarta = document.querySelector(".primeira");
+    segundaCarta = document.querySelector(".segunda");
+    console.log(primeiraCarta);
+    // let imagemVirada1 = carta.querySelector(".front");
+    // let imagemVirada2 = carta.querySelector(".front");
+    // console.log(imagemVirada1)
+    jogadas ++;
+    if (id === id2) {
+        acertos ++;
+        if (acertos === quantidadeDeCartas / 2) {
+            alert("Você ganhou em X jogadas!");
+        }
+        viradas = 0;
+        return true;
+        
+    } else {
+        // setTimeout(function () {
+            
+        //     carta.classList.toggle("esconder");
+            
+        //     primeiraCarta.classList.toggle("primeira");                
+        //     segundaCarta.classList.toggle("segunda");
+        // }, 1000);
+        viradas = 0;
+        return false;
+    }
+    console.log(primeiraCarta)
 }
